5a975e16af69d861c761f443b15a4d5356947203
Maksim <Lukianchukma@gmail.com>
2020-09-20 18:01:13 +0300

    fix: лишний коммент

--- build-tools/utils.js
+++ build-tools/utils.js
@@ -1,14 +1,6 @@
 const path = require('path');
 
-
 const paths = {};
-// const paths = {
-//     root:   path.join(__dirname, '../'),
-//     dist:   path.join(paths.root, 'dist'),
-//     public: path.join(paths.root, 'public'),
-//     res:    path.join(paths.root, 'res'),
-//     src:    path.join(paths.root, 'src')
-// };
 
 paths.root = path.join(__dirname, '../');
 paths.dist = path.join(paths.root, 'dist');
