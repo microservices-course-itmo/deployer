ffd77bf76367a72462cd4f9e8d35630cb28d51fa
Maksim <Lukianchukma@gmail.com>
2020-10-01 22:38:16 +0300

    feat: пофиксил резолв путей

--- build-tools/utils.js
+++ build-tools/utils.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 const paths = {};
 
-paths.root = path.join(__dirname, '../');
+paths.root = path.join(__dirname, '..');
 paths.dist = path.join(paths.root, 'dist');
 paths.public = path.join(paths.root, 'public');
 paths.res = path.join(paths.root, 'res');
