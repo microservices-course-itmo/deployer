863a21174fe2bcdccee601c59300a111bce657cd
Amil <Limash76912@mail.ru>
2020-10-30 09:11:53 +0300

    components of header added

--- src/pages/ApplicationPage/tabs/ApplicationPageTabDeploy/ApplicationPageTabDeploy.tsx
+++ src/pages/ApplicationPage/tabs/ApplicationPageTabDeploy/ApplicationPageTabDeploy.tsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { FormControl, Container, Grid, MenuItem, Select, Button, InputLabel } from '@material-ui/core'
+// import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 import { IApplicationInstance } from '../../../../types/Application'
 
 interface IApplicationPageTabDeployProps {
@@ -7,15 +9,50 @@ interface IApplicationPageTabDeployProps {
   instances?: IApplicationInstance[]
 }
 
+// const useStyles = makeStyles((theme: Theme) =>
+//   createStyles({
+//     formControl: {
+//       margin: theme.spacing(1),
+//       minWidth: 120,
+//     },
+//     selectEmpty: {
+//       marginTop: theme.spacing(2),
+//     },
+//   }),
+// );
+
 export const ApplicationPageTabDeploy = ({
   description,
   templateVersion,
   instances,
 }: IApplicationPageTabDeployProps) => (
   <div>
-    <h3>ApplicationPageTabDeploy</h3>
-    {description}
+    <Container>
+      <Grid container direction='row' justify='space-between' alignItems='flex-start'>
+        <Grid item>
+          <h3>Description: {description}</h3>
+          <h3>Last release: V1.5</h3>
+        </Grid>
+        <Grid item>
+          <InputLabel id='demo-simple-select-filled-label'>Version</InputLabel>
+          <FormControl variant='filled'>
+            <Select labelId='demo-simple-select-filled-label' id='demo-simple-select-filled' value={25}>
+              <MenuItem value=''>
+                <em>None</em>
+              </MenuItem>
+              <MenuItem value={10}>V1.5</MenuItem>
+              <MenuItem value={20}>V1.6</MenuItem>
+              <MenuItem value={30}>V1.7</MenuItem>
+            </Select>
+          </FormControl>
+          <h3>Alias: mongo</h3>
+        </Grid>
+        <Grid item>
+          <Button variant='contained'>Deploy</Button>
+        </Grid>
+      </Grid>
+    </Container>
     {templateVersion}
-    {JSON.stringify(instances)}
+    {console.log(JSON.stringify(instances))}
   </div>
 )
