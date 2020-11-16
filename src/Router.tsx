d8755772416b0bc56483b2941e7167dec3da0582
DanilaPichugin <danilapichugin@hotmail.com>
2020-11-16 15:54:07 +0300

    First Commit

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,12 +2,14 @@ import React from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import { MainPage } from './pages/MainPage/MainPage'
 import { ApplicationPage } from './pages/ApplicationPage/ApplicationPage'
+import { SignIn } from './pages/LoginPage/LoginPage'
 
 const Router = () => (
   <BrowserRouter>
     <Switch>
       <Route path='/app/:name' component={ApplicationPage} />
       <Route path='/' component={MainPage} />
+      <Route path='/registration/:name' component={SignIn} />
     </Switch>
   </BrowserRouter>
 )
