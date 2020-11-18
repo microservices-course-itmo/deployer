71d744074645e0275cbc6aae41db0b165e2efe3a
Amil <Limash76912@mail.ru>
2020-11-18 17:46:00 +0300

    App tabs WIP

--- src/pages/ApplicationPage/tabs/ApplicationPageTabEnvironment/ApplicationPageTabEnvironment.tsx
+++ src/pages/ApplicationPage/tabs/ApplicationPageTabEnvironment/ApplicationPageTabEnvironment.tsx
@@ -1,13 +1,92 @@
 import React from 'react'
+import { makeStyles } from '@material-ui/styles'
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+  Button,
+  TextField,
+  // Checkbox,
+} from '@material-ui/core'
 import { IEnvironmentVariable } from '../../../../types/Application'
 
 interface IApplicationPageTabEnvironmentProps {
   env: IEnvironmentVariable[]
 }
+const useStyles = makeStyles({
+  table: {
+    width: '650px',
+    marginLeft: '30%',
+  },
+  saveBtn: {
+    marginTop: '5px',
+    marginLeft: '30%',
+    padding: '10px 20px',
+  },
+})
 
-export const ApplicationPageTabEnvironment = ({ env }: IApplicationPageTabEnvironmentProps) => (
-  <div>
-    <h3>ApplicationPageTabEnvironment</h3>
-    {JSON.stringify(env)}
-  </div>
-)
+export const ApplicationPageTabEnvironment = ({ env }: IApplicationPageTabEnvironmentProps) => {
+  // const [envs, setEnv] = React.useState(env)
+  // const tempEnvsCreate = (name: string, value: string) => {
+  //   let tempEnvs: IEnvironmentVariable[]
+  //   envs.forEach((element) => {
+  //     if (element[0] === name) {
+  //       tempEnvs.push([name, value])
+  //     } else {
+  //       tempEnvs.push(element)
+  //     }
+  //   })
+  //   setEnv(tempEnvs)
+  // }
+  // const handleChangeEnv = (event: React.ChangeEvent<{ value: unknown }>) => {
+  //   tempEnvsCreate(event.target.name as string, event.target.value as string)
+  // }
+
+  // const handleChangeEnv = (event: React.ChangeEvent<{ value: unknown }>, name:string) => {
+  //   // eslint-disable-next-line
+  //   // @ts-ignore
+  //   setEnvName((envName) => {
+  //     envName.name = (event.target.value as string)
+  //     return(envName)
+  //   }
+  //   //   (prevState) => {
+  //   //   let value = Object.assign({}, prevState.value)
+  //   // }({ ...prevState, [event.target.name]: event.target.value }))
+  // }
+  /* onChange={handleChangeEnv(React.ChangeEvent<{value: unknown}>, en[0]) */
+  const classes = useStyles()
+  return (
+    <div>
+      <h3>ApplicationPageTabEnvironment</h3>
+      <TableContainer className={classes.table} component={Paper}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Name</TableCell>
+              <TableCell align='right'>Value</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {env.map((en) => (
+              <TableRow key={en[0]}>
+                <TableCell>
+                  <TextField disabled id='standard-required' variant='filled' value={en[0]} />
+                </TableCell>
+                <TableCell align='right'>
+                  <TextField id='standard' name={en[0]} variant='filled' value={en[1]} />
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <Button className={classes.saveBtn} variant='contained'>
+        Save
+      </Button>
+    </div>
+  )
+}
--- src/pages/ApplicationPage/tabs/ApplicationPageTabPorts/ApplicationPageTabPorts.tsx
+++ src/pages/ApplicationPage/tabs/ApplicationPageTabPorts/ApplicationPageTabPorts.tsx
@@ -1,13 +1,64 @@
 import React from 'react'
+import { makeStyles } from '@material-ui/styles'
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+  Button,
+  TextField,
+  // Checkbox,
+} from '@material-ui/core'
 import { IPortMapping } from '../../../../types/Application'
 
 interface IApplicationPageTabPortsProps {
   ports: IPortMapping[]
 }
+const useStyles = makeStyles({
+  table: {
+    width: '650px',
+    marginLeft: '30%',
+  },
+  saveBtn: {
+    marginTop: '5px',
+    marginLeft: '30%',
+    padding: '10px 20px',
+  },
+})
 
-export const ApplicationPageTabPorts = ({ ports }: IApplicationPageTabPortsProps) => (
-  <div>
-    <h3>ApplicationPageTabPorts</h3>
-    {JSON.stringify(ports)}
-  </div>
-)
+export const ApplicationPageTabPorts = ({ ports }: IApplicationPageTabPortsProps) => {
+  const classes = useStyles()
+  return (
+    <div>
+      <h3>ApplicationPageTabPorts</h3>
+      <TableContainer className={classes.table} component={Paper}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Name</TableCell>
+              <TableCell align='right'>Value</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {ports.map((port) => (
+              <TableRow key={port[0]}>
+                <TableCell>
+                  <TextField disabled id='standard-required' variant='filled' value={port[0]} />
+                </TableCell>
+                <TableCell align='right'>
+                  <TextField id='standard' name={port[0]} variant='filled' value={port[1]} />
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <Button className={classes.saveBtn} variant='contained'>
+        Save
+      </Button>
+    </div>
+  )
+}
