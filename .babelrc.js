6bb6781ce451909d47da119beaa3902880450f50
DanilaPichugin <danilapichugin@hotmail.com>
2020-11-25 15:34:14 +0300

    fix: Fixed small problems with dependecies and more

--- .babelrc.js
+++ .babelrc.js
@@ -2,6 +2,5 @@
 // babel config for project specified in build-tools/webpack.base.config.js
 
 module.exports = {
-    plugins: ["transform-class-properties"],
     presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript']
 };
