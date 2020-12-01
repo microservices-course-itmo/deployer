02c35f2d122122e22e44b3d3fe67ba5bae86be87
Maksim <Lukianchukma@gmail.com>
2020-12-02 00:07:19 +0300

    Updates

--- .babelrc.js
+++
@@ -1,6 +0,0 @@
-// used only for JEST
-// babel config for project specified in build-tools/webpack.base.config.js
-
-module.exports = {
-    presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript']
-};
--- src/pages/MainPage/MainPage.tsx
+++
@@ -1,3 +0,0 @@
-import React from 'react'
-
-export const MainPage = () => <div>Main Page</div>
