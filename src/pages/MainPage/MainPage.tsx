1b803b4f2777299573179a7e2cd147ad434d0d84
Igrez <56413498+Igrez@users.noreply.github.com>
2020-12-04 17:03:28 +0300

    Add files via upload
    Добавил компоненту при не найденом сервисе, когда сервак встанет нужно будет перенести

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,3 +1,25 @@
 import React from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import { Container } from '@material-ui/core'
 
-export const MainPage = () => <div>Main Page</div>
+const useStyles = makeStyles((theme) => ({
+    page: {
+        marginTop: theme.spacing(8),
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
+}))
+
+export const MainPage = () => {
+    const classes = useStyles()
+
+    return (
+        <Container component='main' maxWidth='xs'>
+            <div className={classes.page}>
+                Данного сервиса не существует
+                <a href='http://localhost:8081/app/deployment-service'>Вернуться назад</a>
+            </div>
+        </Container>
+    )
+}
