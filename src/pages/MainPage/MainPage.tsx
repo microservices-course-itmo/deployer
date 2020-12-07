4eef6715e3e46618d1755a46bba8722fa0042e55
Maksim <Lukianchukma@gmail.com>
2020-12-07 23:00:22 +0300

    catching errors + spinner

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -3,9 +3,10 @@ import { createStyles, makeStyles } from '@material-ui/core/styles'
 import List from '@material-ui/core/List'
 import ListItem, { ListItemProps } from '@material-ui/core/ListItem'
 import ListItemText from '@material-ui/core/ListItemText'
-import { Container, Grid, TextField } from '@material-ui/core'
+import { CircularProgress, Container, Grid, TextField } from '@material-ui/core'
 import { Link } from 'react-router-dom'
 import { useQuery } from 'react-query'
+import Alert from '@material-ui/lab/Alert'
 
 const useStyles = makeStyles(() =>
   createStyles({
@@ -49,7 +50,11 @@ export const MainPage = () => {
     fetch(`${process.env.API}/application/names`)
       .then((res) => res.json())
       .then((items) => {
-        setSearchItems(items)
+        if (items.length) {
+          setSearchItems(items)
+        } else {
+          throw new Error('Not Found')
+        }
         return items
       })
   )
@@ -60,19 +65,12 @@ export const MainPage = () => {
     setSearchItems(filterList(value as string, data!))
   }
 
-  if (isLoading) {
-    return <span>Loading...</span>
-  }
-
-  if (isError) {
-    return <span>Error</span>
-  }
-
   return (
     <Container className={classes.main}>
       <Grid container direction='column' justify='space-around' alignItems='center'>
         <div className={classes.root}>
           <TextField
+            disabled={isError || isLoading}
             className={classes.input}
             label='Search input'
             margin='normal'
@@ -81,15 +79,21 @@ export const MainPage = () => {
             value={inputValue}
             onChange={handleValueChange}
           />
-          <List component='nav' aria-label='main mailbox folders'>
-            {searchItems.map((item) => (
-              <ListItemLink to={`/app/${item}`} key={item}>
-                <ListItemText primary={item} />
-              </ListItemLink>
-            ))}
+          {isError ? (
+            <Alert severity='error'>This is an error alert — check it out!</Alert>
+          ) : isLoading ? (
+            <CircularProgress />
+          ) : (
+            <List component='nav' aria-label='main mailbox folders'>
+              {searchItems.map((item) => (
+                <ListItemLink to={`/app/${item}`} key={item}>
+                  <ListItemText primary={item} />
+                </ListItemLink>
+              ))}
 
-            {!searchItems.length && <span>no search results</span>}
-          </List>
+              {!searchItems.length && <span>no search results</span>}
+            </List>
+          )}
         </div>
       </Grid>
     </Container>
