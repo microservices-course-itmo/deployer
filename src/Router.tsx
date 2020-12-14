f2cf92b5bc73ee1143cac5ab1ea900854b2fce10
Maksim <Lukianchukma@gmail.com>
2020-12-15 00:05:58 +0300

    прикрутили червак йоу

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
-import { SettingsList } from './pages/SettingsPage/SettingsList'
 import { MainPage } from './pages/MainPage/MainPage'
 import { ApplicationPage } from './pages/ApplicationPage/ApplicationPage'
 import { SignIn } from './pages/LoginPage/LoginPage'
+import { Settings } from './pages/SettingsPage/Settings'
 
 const Router = () => (
   <BrowserRouter>
     <Switch>
-      <Route exact path='/settings' component={SettingsList} />
+      <Route exact path='/settings' component={Settings} />
       <Route exact path='/login' component={SignIn} />
       <Route path='/app/:name' component={ApplicationPage} />
       <Route path='/' component={MainPage} />
--- src/pages/SettingsPage/SettingsList.tsx
+++ src/pages/SettingsPage/SettingsList.tsx
@@ -1,6 +1,7 @@
-import { Container, Grid, TextField, Theme } from '@material-ui/core'
+import { Button, Container, Grid, TextField, Theme } from '@material-ui/core'
 import { createStyles, makeStyles } from '@material-ui/core/styles'
-import React, { useState } from 'react'
+import React, { ChangeEvent, useState } from 'react'
+import { useMutation } from 'react-query'
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -34,6 +35,28 @@ export const SettingsList = ({ settings }: ISettingsListProps) => {
   const classes = useStyles()
   const [settingItems, setSettingItems] = useState<{ [key: string]: string }>(settings)
 
+  const onChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const {
+      target: { name, value },
+    } = event
+    setSettingItems((prevState) => ({ ...prevState, [name]: value }))
+  }
+
+  const [mutate] = useMutation((newSettings: { [key: string]: string }) => {
+    return fetch(`${process.env.API}/settings/set`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept-Encoding': 'gzip, deflate',
+      },
+      body: JSON.stringify(newSettings),
+    }).then((resp) => resp.json())
+  })
+
+  const onSave = () => {
+    mutate(settingItems)
+  }
+
   return (
     <Container className={classes.main}>
       <Grid container direction='column' justify='space-around' alignItems='center'>
@@ -43,7 +66,13 @@ export const SettingsList = ({ settings }: ISettingsListProps) => {
               <h1>Docker Adress</h1>
             </Grid>
             <Grid>
-              <TextField id='outlined-basic' variant='outlined' />
+              <TextField
+                id='outlined-basic'
+                variant='outlined'
+                value={settingItems['dockerAddress']}
+                name='dockerAddress'
+                onChange={onChange}
+              />
             </Grid>
           </Grid>
           <Grid container spacing={1} alignItems='center'>
@@ -51,7 +80,13 @@ export const SettingsList = ({ settings }: ISettingsListProps) => {
               <h1>Version Regestry</h1>
             </Grid>
             <Grid>
-              <TextField id='outlined-basic' variant='outlined' />
+              <TextField
+                id='outlined-basic'
+                variant='outlined'
+                value={settingItems['versionRegistry']}
+                name='versionRegistry'
+                onChange={onChange}
+              />
             </Grid>
           </Grid>
           <Grid container spacing={1} alignItems='center'>
@@ -59,9 +94,16 @@ export const SettingsList = ({ settings }: ISettingsListProps) => {
               <h1>Image Regestry</h1>
             </Grid>
             <Grid>
-              <TextField id='outlined-basic' variant='outlined' />
+              <TextField
+                id='outlined-basic'
+                variant='outlined'
+                value={settingItems['imageRegistry']}
+                name='imageRegistry'
+                onChange={onChange}
+              />
             </Grid>
           </Grid>
+          <Button onClick={onSave}>Save</Button>
         </div>
       </Grid>
     </Container>
