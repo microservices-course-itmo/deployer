0249f86ef1975676bb8a318e3bb2af7a17b21126
Maksim <Lukianchukma@gmail.com>
2021-03-18 20:03:33 +0300

    закинул проифиль + авторизацию запросов

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,21 +1,46 @@
-import React from 'react'
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom'
 import { MainPage } from './pages/MainPage/MainPage'
 import { ApplicationPage } from './pages/ApplicationPage/ApplicationPage'
 import { SignIn } from './pages/LoginPage/LoginPage'
 import { Settings } from './pages/SettingsPage/Settings'
 import { NewAppPage } from './pages/NewAppPage/NewAppPage'
+import { ProfilePage } from './pages/Profile/ProfilePage'
 
-const Router = () => (
-  <BrowserRouter>
-    <Switch>
-      <Route exact path='/new-app' component={NewAppPage} />
-      <Route exact path='/settings' component={Settings} />
-      <Route exact path='/login' component={SignIn} />
-      <Route path='/app/:name' component={ApplicationPage} />
-      <Route path='/' component={MainPage} />
-    </Switch>
-  </BrowserRouter>
-)
+const Router = () => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false)
+  const [isAuthenticationChecked, setIsAuthenticationChecked] = useState(false)
+
+  useEffect(() => {
+    const accessToken = window.localStorage.getItem('accessToken')
+
+    if (accessToken) {
+      fetch(`http://77.234.215.138:18080/user-service/validate?token=${accessToken}`, {
+        method: 'POST',
+      })
+        .then(() => {
+          setIsAuthenticated(true)
+        })
+        .finally(() => {
+          setIsAuthenticationChecked(true)
+        })
+    } else {
+      setIsAuthenticationChecked(true)
+    }
+  }, [])
+
+  return isAuthenticationChecked ? (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path='/new-app' render={() => (isAuthenticated ? <NewAppPage /> : <Redirect to='/login' />)} />
+        <Route exact path='/settings' render={() => (isAuthenticated ? <Settings /> : <Redirect to='/login' />)} />
+        <Route exact path='/login' component={SignIn} />
+        <Route exact path='/profile' render={() => (isAuthenticated ? <ProfilePage /> : <Redirect to='/login' />)} />
+        <Route path='/app/:name' render={() => (isAuthenticated ? <ApplicationPage /> : <Redirect to='/login' />)} />
+        <Route path='/' render={() => (isAuthenticated ? <MainPage /> : <Redirect to='/login' />)} />
+      </Switch>
+    </BrowserRouter>
+  ) : null
+}
 
 export default Router
---
+++ src/pages/Profile/ProfilePage.tsx
@@ -0,0 +1,75 @@
+import React, { useEffect, useState } from 'react'
+import { Container, CssBaseline } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles'
+import { useHistory } from 'react-router-dom'
+import { Appbar } from '../Appbar/Appbar'
+
+const useStyles = makeStyles(() => ({
+  paper: {
+    textAlign: 'center',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    marginTop: '88px',
+  },
+}))
+
+export const ProfilePage = () => {
+  const classes = useStyles()
+
+  const history = useHistory()
+
+  const [userData, setUserData] = useState<null | {
+    name: string
+    birthdate: string
+    id: string
+    phoneNumber: string
+    role: string
+  }>(null)
+
+  useEffect(() => {
+    const accessToken = window.localStorage.getItem('accessToken')
+
+    if (accessToken) {
+      fetch('http://77.234.215.138:18080/user-service/users/me', {
+        method: 'GET',
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          setUserData(data)
+        })
+        .catch(() => {
+          history.replace('/login')
+        })
+    } else {
+      history.replace('/login')
+    }
+  }, [])
+
+  return (
+    <>
+      <Appbar />
+      <Container component='main' maxWidth='xs'>
+        <CssBaseline />
+        <div className={classes.paper}>
+          <div>
+            <h3>ID</h3>
+            <h4>{userData?.id || ''}</h4>
+            <br />
+            <h3>Phone Number</h3>
+            <h4>{userData?.phoneNumber || ''}</h4>
+            <br />
+            <h3>Name</h3>
+            <h4>{userData?.name || ''}</h4>
+            <br />
+            <h3>Birth date</h3>
+            <h4>{userData?.birthdate || ''}</h4>
+          </div>
+        </div>
+      </Container>
+    </>
+  )
+}
