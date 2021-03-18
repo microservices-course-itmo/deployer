511c4ba7ecabde07ca541c1694592ea452827c85
mm.pavlov <mm.pavlov@okko.tv>
2021-03-18 23:31:47 +0700

    hotfix delete application button

--- src/api/applicationController.ts
+++ src/api/applicationController.ts
@@ -3,4 +3,11 @@ const getRoute = (path: string) => `${process.env.API}${path}`
 export const removeInstance = (id: string) =>
   fetch(getRoute(`/application/delete/byId/${id}`), {
     method: 'DELETE',
+    headers: {
+        'Content-Type': 'application/json',
+        'Accept-Encoding': 'gzip, deflate',
+      },
+      body: JSON.stringify({
+        id
+      }),
   }).then((resp) => resp.json())
